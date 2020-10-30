30c47b1753e3d55d2452c3ccaaee29d4131c7fd5
Serena <serena.gracco@virgilio.it>
2020-10-30 19:05:16 +0100

    Commit with new buttons, added fire screen and new buttons

--- app/src/main/java/com/example/serena/pokweaks/FireType.java
+++ app/src/main/java/com/example/serena/pokweaks/FireType.java
@@ -9,5 +9,8 @@ public class FireType extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_type);
+
+
+
     }
 }
